b345841c9cacbaac080c7ea7431a754c0419fb46
jshjelse <jshjelse@stud.ntnu.no>
2026-08-06 21:00:44 +0200

    Organize React project structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,7 @@
-import { useEffect, useState } from "react";
+import Home from "./pages/Home"
 
 function App() {
-  const [ message, setMessage ] = useState("");
-
-  useEffect(() => {
-    const API_URL = import.meta.env.VITE_API_URL;
-
-    fetch(`${API_URL}/api/hello/`).then(
-      response => response.json()
-    ).then(
-      data => {
-        setMessage(data.message);
-      }
-    );
-  }, []);
-
-  return (
-    <div>
-      <h1>Orienteringsanalyse</h1>
-
-      <p>{ message }</p>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+function Home() {
+  const [ message, setMessage ] = useState("");
+
+  useEffect(() => {
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    fetch(`${API_URL}/api/hello/`).then(
+      response => response.json()
+    ).then(
+      data => {
+        setMessage(data.message);
+      }
+    );
+  }, []);
+
+  return (
+    <div>
+      <h1>Orienteringsanalyse</h1>
+
+      <p>{ message }</p>
+    </div>
+  );
+}
+
+export default Home;
